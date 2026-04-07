3b321e0542d98f06dd4dcc876687727974e51eca
Isometrico <mggv448@gmail.com>
2026-04-07 17:26:03 -0600

    Added font, UI Styling and the chosen colors for our project; missing styling for Trip and Activity Editor

--- app/src/main/java/com/example/wanderer/ui/theme/Color.kt
+++ app/src/main/java/com/example/wanderer/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.example.wanderer.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val GreenMain = Color(0xFF03E07A)
+val GreenDark = Color(0xFF16443B)
+val BlueGrey = Color(0xFF2D394B)
+val BlueBackground = Color(0xFF151A22)
+val WhiteMain = Color(0xFFF0F1F3)
