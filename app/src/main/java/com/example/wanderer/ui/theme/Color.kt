4b8fd5927e45f5a450df0a30994d633864670b49
jsnyder13 <157261258+jsnyde13git@users.noreply.github.com>
2026-04-09 08:47:21 -0600

    Revert "Merge UI-Theming into jr3"

--- app/src/main/java/com/example/wanderer/ui/theme/Color.kt
+++ app/src/main/java/com/example/wanderer/ui/theme/Color.kt
@@ -2,8 +2,10 @@ package com.example.wanderer.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val GreenMain = Color(0xFF03E07A)
-val GreenDark = Color(0xFF16443B)
-val BlueGrey = Color(0xFF2D394B)
-val BlueBackground = Color(0xFF151A22)
-val WhiteMain = Color(0xFFF0F1F3)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
